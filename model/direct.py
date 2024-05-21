851e9b049397d4d7c342e6451d4e6abd737daffe
defabdullah <ahmet.susuz@boun.edu.tr>
2024-05-21 23:09:17 +0300

    update models

--- model/direct.py
+++ model/direct.py
@@ -1,11 +1,33 @@
 import tensorflow as tf
+from transformers import TFDistilBertModel
 
 class DirectModel(tf.keras.Model):
     def __init__(self, character_bert, vocab_size):
         super(DirectModel, self).__init__()
         self.character_bert = character_bert
-        self.output_layer = tf.keras.layers.Dense(vocab_size, activation='softmax')
+        self.bert_layer = TFDistilBertModel.from_pretrained('distilbert-base-uncased')
+        self.dense = tf.keras.layers.Dense(vocab_size)
 
-    def call(self, inputs):
-        embeddings = self.character_bert(inputs)[0]  # Getting the last hidden state
-        return self.output_layer(embeddings)
+    def call(self, inputs, training=False):
+        # Extract input_ids and attention_mask from inputs dictionary
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
+        
+        # Get the embeddings from BERT
+        bert_outputs = self.bert_layer(input_ids, attention_mask=attention_mask)
+        sequence_output = bert_outputs.last_hidden_state  # [batch_size, sequence_length, hidden_size]
+        
+        logits = self.dense(sequence_output)
+        return logits
+
+    def get_config(self):
+        config = super(DirectModel, self).get_config()
+        config.update({
+            "character_bert": self.character_bert,
+            "vocab_size": self.dense.units
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
--- model/fused.py
+++ model/fused.py
@@ -1,17 +1,42 @@
 import tensorflow as tf
+from transformers import TFDistilBertModel
 
 class FusedModel(tf.keras.Model):
     def __init__(self, character_bert, encoder, decoder, vocab_size, d_model):
         super(FusedModel, self).__init__()
         self.character_bert = character_bert
-        self.projection_layer = tf.keras.layers.Dense(d_model)  # Projection to match encoder input size
         self.encoder = encoder
         self.decoder = decoder
-        self.final_layer = tf.keras.layers.Dense(vocab_size, activation='softmax')
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+        self.bert_layer = TFDistilBertModel.from_pretrained('distilbert-base-uncased')
+        self.projection_layer = tf.keras.layers.Dense(d_model)
+        self.dense = tf.keras.layers.Dense(vocab_size)
 
-    def call(self, inputs):
-        char_embeddings = self.character_bert(inputs)[0]
-        projected_embeddings = self.projection_layer(char_embeddings)
-        encoder_output = self.encoder(projected_embeddings)
-        decoder_output = self.decoder(encoder_output)
-        return self.final_layer(decoder_output)
+    def call(self, inputs, training=False):
+        # Get the embeddings from BERT
+        bert_outputs = self.bert_layer(inputs)
+        sequence_output = bert_outputs.last_hidden_state  # [batch_size, sequence_length, hidden_size]
+        
+        # Project embeddings to the required dimension
+        sequence_output = self.projection_layer(sequence_output)
+
+        enc_output = self.encoder(sequence_output, training=training)
+        dec_output = self.decoder(enc_output, training=training)
+        logits = self.dense(dec_output)
+        return logits
+
+    def get_config(self):
+        config = super(FusedModel, self).get_config()
+        config.update({
+            "character_bert": self.character_bert,
+            "encoder": self.encoder,
+            "decoder": self.decoder,
+            "vocab_size": self.vocab_size,
+            "d_model": self.d_model
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
