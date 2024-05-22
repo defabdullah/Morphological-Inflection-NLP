06c8661a36d0d90439861db4659f780b2f3b49d5
defabdullah <ahmet.susuz@boun.edu.tr>
2024-05-22 03:28:54 +0300

    add scripts

---
+++ scripts/nlp_train_3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --container-image ghcr.io\#cmpe-491/abdullah-morphological-inflection-nlp:latest
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=40G
+
+source /opt/python3/venv/base/bin/activate
+python3 '/opt/python3/venv/base/morphological-inflection-nlp/train.py' --output_dir '/users/ahmet.susuz/morph/' --train_file '/users/ahmet.susuz/eng_train.txt' --val_file '/users/ahmet.susuz/eng_val.txt' --epoch 200 --batch_size 32 --learning_rate 1e-5 --d_model 512 --num_heads 12 --dff 512 --dropout_rate 0.2
